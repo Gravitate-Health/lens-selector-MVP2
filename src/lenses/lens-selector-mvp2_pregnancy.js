d3429e946f1af77b243dc8eff27fe245c6862410
10alejospain <10alejo.est@gmail.com>
2023-06-27 13:03:29 +0200

    pregnancy lense corrected to standar - WIP

--- src/lenses/lens-selector-mvp2_pregnancy.js
+++ src/lenses/lens-selector-mvp2_pregnancy.js
@@ -17,7 +17,8 @@ let annotateHTMLsection = (listOfCategories, enhanceTag) => {
     });
     //Never return empty section check
     if (response == null || response == "") {
-     return htmlData
+        throw new Error('Annotation proccess failed: Returned empty or null response');
+        //return htmlData
     }
     else {
      return response
@@ -25,10 +26,16 @@ let annotateHTMLsection = (listOfCategories, enhanceTag) => {
 }
 
 let  enhance = () => {
-    let listOfCategoriesToSearch = ["pregnancyCategory", "breastfeedingCategory", "contra-indication-pregancy"]
+    //                  pregnancyCategory    breastfeedingCategory
+    //                              ICPC2    SNOMED               
+    let listOfCategoriesToSearch = ["W78", "69840006", ]//"contra-indication-pregancy"]
 
     // Get IPS gender and check if is female 
     let gender;
+
+    if (ips == "" || ips == null) {
+        throw new Error('Failed to load IPS: the LEE is getting a empty IPS');
+    }
     ips.entry.forEach(element => {
         if (element.resource.resourceType == "Patient") {
             gender = element.resource.gender
@@ -37,22 +44,35 @@ let  enhance = () => {
                 return htmlData
             }
         }
-   });
-   //Get condition categories for the ePI and filters by the condition we want to check
-   let categories = []
-   epi.entry.forEach(element => {
-        if (element.resource.extension != undefined) {
-            element.resource.extension.forEach(category => {
-                console.log("Category found: "+ category.extension[0].valueString)
-                if (listOfCategoriesToSearch.includes(category.extension[0].valueString)) {
-                    categories.push(category.extension[0].valueString)
+    });
+
+    // ePI traslation from terminology codes to their human redable translations in the sections
+    let compositions = 0;
+    let categories = [];
+    epi.entry.forEach(entry => {
+        if (entry.resource.resourceType == "Composition") {
+            compositions++;
+            //Iterated through the Condition element searching for conditions
+            entry.resource.extension.forEach(element => {
+                // Check if the position of the extension[1] is correct
+                if (element.extension[1].url == "concept") {
+                        // Search through the different terminologies that may be avaible to check in the condition
+                        element.extension[1].valueCodeableReference.concept.coding.forEach(termilogySystem => {
+                            // Adds the category to be hightlighted in the epi
+                            if (listOfCategoriesToSearch.find(termilogySystem.code)) {
+                                categories.push(termilogySystem.display)
+                            }
+                        });
                 }
             });
         }
-   });
-   console.log(categories)
+    });
+    if (compositions = 0) {
+        throw new Error('Bad ePI: no category "Composition" found');
+    }
+
    if (!categories.length) {
-    return htmlData
+    throw new Error('No categories found',categories);
    }
    //Focus (adds highlight class) the html applying every category found
    return annotateHTMLsection(categories, "highlight")
