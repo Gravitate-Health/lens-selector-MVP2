cebdf9fcede75998f8f55a1aa1f6f2fa7e21b504
Alejandro Alonso López <aalonsopersonal@gmail.com>
2023-07-13 11:22:07 +0200

    VIH lense now fully working (IPS interaction included)

--- src/lenses/lens-selector-mvp2_HIV.js
+++ src/lenses/lens-selector-mvp2_HIV.js
@@ -19,73 +19,78 @@ let annotateHTMLsection = (listOfCategories, enhanceTag) => {
     });
     //Never return empty section check
     if (response == null || response == "") {
-        throw new Error('Annotation proccess failed: Returned empty or null response');
+        throw new Error(
+            "Annotation proccess failed: Returned empty or null response"
+        );
+        //return htmlData
     } else {
         return response;
     }
 };
 
-let haveHIV = (ips) => {
-    ipsEntries = ips.entry;
+let enhance = () => {
+    let listOfCategoriesToSearch = ["B90", "86406008"];
 
-    ipsEntries.forEach((entry) => {
-        if (entry.resource.resourceType == "Condition") {
-            resourceCodings = entry.resource.code.coding;
-            resourceCodings.forEach((coding) => {
-                if (coding.code == "86406008") {
-                    // This code is only valid if the system is SNOMED-CT
-                    return true;
-                }
-            });
+    if (ips == "" || ips == null) {
+        throw new Error("Failed to load IPS: the LEE is getting a empty IPS");
+    }
+
+    //Search for the condition in the IPS
+    let patientHasCondition = false;
+    ips.entry.forEach((element) => {
+        if (element.resource.resourceType == "Condition") {
+            if (element.resource.code != undefined) {
+                element.resource.code.coding.forEach((coding) => {
+                    if (listOfCategoriesToSearch.includes(coding.code)) {
+                        patientHasCondition = true;
+                    }
+                });
+            }
         }
     });
 
-    return false;
-};
-
-
-
-let enhance = () => {
-    // Check if the patient has HIV
-    if (!haveHIV(ips)) {
-        return htmlData
+    if (!patientHasCondition) {
+        return htmlData;
     }
 
-    let listOfCategoriesToSearch = ["B90"]; // This is the code for AIDS in ICPC-2
-
+    // ePI traslation from terminology codes to their human redable translations in the sections
     let compositions = 0;
     let categories = [];
-
-    epi.entry.forEach(entry => {
+    epi.entry.forEach((entry) => {
         if (entry.resource.resourceType == "Composition") {
             compositions++;
             //Iterated through the Condition element searching for conditions
-            entry.resource.extension.forEach(element => {
+            entry.resource.extension.forEach((element) => {
+                
                 // Check if the position of the extension[1] is correct
                 if (element.extension[1].url == "concept") {
                     // Search through the different terminologies that may be avaible to check in the condition
-                    if (element.extension[1].valueCodeableReference.coding != null) {
-                        element.extension[1].valueCodeableReference.coding.forEach(coding => {
-                            // Check if the code is in the list of categories to search
-                            if (listOfCategoriesToSearch.includes(coding.code)) {
-                                // Check if the category is already in the list of categories
-                                categories.push(coding.code)
+                    if (element.extension[1].valueCodeableReference.concept != undefined) {
+                        element.extension[1].valueCodeableReference.concept.coding.forEach(
+                            (coding) => {
+                                console.log("Extension: " + element.extension[0].valueString + ":" + coding.code)
+                                // Check if the code is in the list of categories to search
+                                if (listOfCategoriesToSearch.includes(coding.code)) {
+                                    // Check if the category is already in the list of categories
+                                    categories.push(element.extension[0].valueString);
+                                }
                             }
-                        });
+                        );
                     }
                 }
             });
         }
     });
+
+    console.log("Categories: " + categories);
+
     if (compositions == 0) {
         throw new Error('Bad ePI: no category "Composition" found');
     }
 
-    if (!categories.length) {
-        console.log(categories);
-        return htmlData;
+    if (categories.length == 0) {
+        throw new Error("No categories found", categories);
     }
-
     //Focus (adds highlight class) the html applying every category found
     return annotateHTMLsection(categories, "highlight");
 };
