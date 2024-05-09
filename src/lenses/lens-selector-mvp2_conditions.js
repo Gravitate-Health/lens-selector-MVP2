7c9efa2f2b75c9f0498927811f757e3b567a8265
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-05-09 16:21:48 +0200

    Conditions Lens - change code structure to make it more human readable

--- src/lenses/lens-selector-mvp2_conditions.js
+++ src/lenses/lens-selector-mvp2_conditions.js
@@ -4,53 +4,6 @@ let htmlData = html;
 let epiData = epi;
 let ipsData = ips;
 
-let annotationProcess = (listOfCategories, enhanceTag, document, response) => {
-    listOfCategories.forEach((check) => {
-        if (response.includes(check)) {
-            let elements = document.getElementsByClassName(check);
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(enhanceTag);
-            }
-            if (document.getElementsByTagName("head").length > 0) {
-                document.getElementsByTagName("head")[0].remove();
-            }
-            if (document.getElementsByTagName("body").length > 0) {
-                response = document.getElementsByTagName("body")[0].innerHTML;
-                console.log("Response: " + response);
-            } else {
-                console.log("Response: " + document.documentElement.innerHTML);
-                response = document.documentElement.innerHTML;
-            }
-        }
-    });
-
-    if (response == null || response == "") {
-        throw new Error(
-            "Annotation proccess failed: Returned empty or null response"
-        );
-        //return htmlData
-    } else {
-        console.log("Response: " + response);
-        return response;
-    }
-}
-
-let annotateHTMLsection = async (listOfCategories, enhanceTag) => {
-    let response = htmlData;
-    let document;
-
-    if (typeof window === "undefined") {
-        let jsdom = await import("jsdom");
-        let { JSDOM } = jsdom;
-        let dom = new JSDOM(htmlData);
-        document = dom.window.document;
-        return annotationProcess(listOfCategories, enhanceTag, document, response);
-    } else {
-        document = window.document;
-        return annotationProcess(listOfCategories, enhanceTag, document, response);
-    }
-};
-
 let enhance = async () => {
     // Proves that IPS exists
     if (ips == "" || ips == null) {
@@ -116,6 +69,54 @@ let enhance = async () => {
     //Focus (adds highlight class) the html applying every category found
     return await annotateHTMLsection(categories, "highlight");
 };
+
+let annotationProcess = (listOfCategories, enhanceTag, document, response) => {
+    listOfCategories.forEach((check) => {
+        if (response.includes(check)) {
+            let elements = document.getElementsByClassName(check);
+            for (let i = 0; i < elements.length; i++) {
+                elements[i].classList.add(enhanceTag);
+            }
+            if (document.getElementsByTagName("head").length > 0) {
+                document.getElementsByTagName("head")[0].remove();
+            }
+            if (document.getElementsByTagName("body").length > 0) {
+                response = document.getElementsByTagName("body")[0].innerHTML;
+                console.log("Response: " + response);
+            } else {
+                console.log("Response: " + document.documentElement.innerHTML);
+                response = document.documentElement.innerHTML;
+            }
+        }
+    });
+
+    if (response == null || response == "") {
+        throw new Error(
+            "Annotation proccess failed: Returned empty or null response"
+        );
+        //return htmlData
+    } else {
+        console.log("Response: " + response);
+        return response;
+    }
+}
+
+let annotateHTMLsection = async (listOfCategories, enhanceTag) => {
+    let response = htmlData;
+    let document;
+
+    if (typeof window === "undefined") {
+        let jsdom = await import("jsdom");
+        let { JSDOM } = jsdom;
+        let dom = new JSDOM(htmlData);
+        document = dom.window.document;
+        return annotationProcess(listOfCategories, enhanceTag, document, response);
+    } else {
+        document = window.document;
+        return annotationProcess(listOfCategories, enhanceTag, document, response);
+    }
+};
+
 return {
     enhance: enhance,
     getSpecification: getSpecification,
