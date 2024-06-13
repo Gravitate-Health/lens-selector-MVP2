995a3f0d9d07cf83d8d111a46545b04661ba3b51
Alejandro Alonso López <aalonsopersonal@gmail.com>
2024-06-13 14:25:28 +0200

    Adaptation of the lens-selector to use IPS FHIR server for lens retrieval

--- src/controllers/lensesController.ts
+++ src/controllers/lensesController.ts
@@ -2,6 +2,8 @@ import { Response, Request } from "express";
 import { Logger } from "../utils/Logger";
 import fs, { readFile, readdir } from "fs"
 
+const fhirIpsURL = process.env.BASE_URL + "/ips/api/fhir"
+
 const readLensesDir = async () => {
   try {
     let path = `${process.cwd()}/build/lenses/`
@@ -24,29 +26,36 @@ const readLensFile = async (path: string) => {
 
 const retrieveLensesNames = async () => {
   try {
-    let lensesNames = await readLensesDir()
-    console.log(`Found the following lenses: ${lensesNames || "None"}`);
-    return await lensesNames
+    let response = []
+    fetch(`${fhirIpsURL}/Library`)
+      .then(res => res.json())
+      .then(bundle => {
+        for (let entry of bundle.entry) {
+          response.push(entry.resource.name)
+        }
+      })
   } catch (error) {
     console.log(error);
     return null
   }
 }
 
-const retrieveLense = async (lenseId: string) => {
-  let lensFilename: string = lenseId + ".js"
-  let lensPath = `${process.cwd()}/build/lenses/${lensFilename}`
-
-  let lensesNames = await retrieveLensesNames()
-  console.log(`Looking for lens: ${lensFilename}`);
-  if (lensesNames?.includes(lensFilename)) {
-    try {
-      return await readLensFile(lensPath)
-    } catch (error) {
-      console.log(error);
-    }
-  } else {
-    return null
+const retrieveLense = async (lenseId: string): Promise<{lensResource: Object, lens: string} | null> => {
+  let lensPath = `/Library?name:exact=${lenseId}`
+  try {
+    return await fetch(`${fhirIpsURL}${lensPath}`)
+      .then(res => res.json())
+      .then(bundle => {
+        const dataBase64 = bundle.entry[0].resource.content[0].data
+        const data = atob(dataBase64)
+        return {
+          lensResource: bundle.entry[0].resource,
+          lens: data
+        }
+      })
+  } catch (error) {
+    console.log(error);
+    return null 
   }
 }
 
@@ -60,9 +69,18 @@ export const getLens = async (req: Request, res: Response) => {
   }
   try {
     let lens = await retrieveLense(reqlens)
-    let lensObject = {
-      metadata: {},
-      lens: lens
+    let lensObject
+    
+    if (lens === null) {
+      console.log(`Lens ${reqlens} not found.`);
+      res.status(404).send({
+        message: `Lens ${reqlens} not found.`
+      })
+    } else {
+      lensObject = {
+        lens: lens.lens,
+        lensResource: lens.lensResource
+      }
     }
     console.log(`Sending lens: ${JSON.stringify(lensObject)}`);
     res.status(200).send(lensObject)
